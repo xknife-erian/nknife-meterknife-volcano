c49a4dc926701cb11ac38fc5ab24694720b16e1e
lukan <lukan@live.cn>
2024-11-12 21:07:40 +0800

    更新仓储类型检查逻辑并添加日志记录
    增加了NLog的引用，并在`RepositoriesAutofacModule`类中添加了一个静态的日志记录器`s_logger`。
    增加了一个静态的`RepositoryTypes`列表，用于存储符合条件的仓储类型。
    修改了`ConfiguresRepository`方法的逻辑：
      * 之前的逻辑是检查类型是否是`BaseRdbInternalRepository<>`的非抽象、非泛型子类。
      * 现在的逻辑是检查类型是否实现了`IRepository`接口。
      * 如果类型实现了`IRepository`接口，会记录一条跟踪日志。
      * 进一步检查类型是否是非抽象、非泛型的，如果是，则获取该类型实现的所有接口。
      * 构造一个泛型接口`IRdbRepository<,>`的类型定义，并检查类型实现的接口中是否有任何一个与该泛型接口定义匹配。
      * 如果匹配，则将该类型添加到`RepositoryTypes`列表中，并记录一条信息日志。

--- Modules/Data/NKnife.Module.Data.Storages/RepositoriesAutofacModule.cs
+++ Modules/Data/NKnife.Module.Data.Storages/RepositoriesAutofacModule.cs
@@ -1,19 +1,37 @@
 ﻿using Autofac;
 using NKnife.Module.Data.Storages.Internal;
+using NLog;
 using RAY.Storages;
 
 namespace NKnife.Module.Data.Storages
 {
     public class RepositoriesAutofacModule : Autofac.Module
     {
+        private static readonly Logger s_logger = LogManager.GetCurrentClassLogger();
+
         internal static List<Type> RepositoryTypes { get; } = [];
 
         public static void ConfiguresRepository(Type type)
         {
-            if (typeof(BaseRdbInternalRepository<>).IsAssignableFrom(type)
-                && type is { IsAbstract: false, IsGenericType: false })
+            if (typeof(IRepository).IsAssignableFrom(type))
             {
-                RepositoryTypes.Add(type);
+                s_logger.Trace($"{type.Name} is {nameof(IRepository)}");
+
+                if (type is { IsAbstract: false, IsGenericType: false })
+                {
+                    // 获取所有实现的接口
+                    var interfaces = type.GetInterfaces();
+
+                    // 构造泛型接口的类型定义
+                    var genericInterfaceDefinition = typeof(IRdbRepository<,>);
+
+                    // 检查是否有任何一个接口与给定的泛型接口定义匹配
+                    if (interfaces.Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericInterfaceDefinition))
+                    {
+                        RepositoryTypes.Add(type);
+                        s_logger.Info($"{type.Name} registered to IoC.");
+                    }
+                }
             }
         }
 
