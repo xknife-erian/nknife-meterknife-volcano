3d8dfbf1ebf82a7eb0bf32bf2f6e5364f7309ff7
lukan <lukan@live.cn>
2024-12-20 00:17:58 +0800

    更新 ConnectorManagerView 和 ViewModel 以改进 UI 和数据绑定
    对 ConnectorManagerView.xaml 文件进行了修改：
    - 将顶部的 StackPanel 高度从 32 改为 28，并添加了 x:Name 和 Background 属性。
    - 修改了 StackPanel.Resources 中的样式定义，从 Button 的样式改为 ToolBar 的样式，并自定义了 ToolBar 的模板。
    - 将原来的按钮替换为 ToolBar，并在 ToolBar 中添加了 Menu 和 MenuItem，用于“新建”和“删除”操作。
    - 将底部的 DataGrid 替换为 ListView，并绑定到 ConnectorCollection 数据源，同时自定义了 ListView 的显示模板，包括图片和描述文本。
    
    对 ConnectorManagerViewModel.cs 文件进行了修改：
    - 添加了 System.Collections.ObjectModel、CommunityToolkit.Mvvm.ComponentModel 和 NKnife.Circe.Base 的引用。
    - 在 ConnectorManagerViewModel 类中添加了 ConnectorCollection 属性，并在构造函数中初始化了一个示例数据。
    - 新增了 ConnectorInfo 类，继承自 ObservableObject，包含 Id、ImagePath 和 Description 属性，并实现了属性变更通知。
    
    对 NKnife.Feature.ConnectorManager.csproj 文件进行了修改：
    - 添加了对 NKnife.Circe.Base 项目的引用。

--- Features/NKnife.Feature.ConnectorManager/Internal/ViewModel/ConnectorManagerViewModel.cs
+++ Features/NKnife.Feature.ConnectorManager/Internal/ViewModel/ConnectorManagerViewModel.cs
@@ -1,8 +1,43 @@
-﻿using RAY.Windows.Common.ViewModels.Layout;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using NKnife.Circe.Base;
+using RAY.Windows.Common.ViewModels.Layout;
 
 namespace NKnife.Feature.ConnectorManager.Internal.ViewModel
 {
     class ConnectorManagerViewModel : BaseToolPaneViewModelV1
     {
+        public ConnectorManagerViewModel()
+        {
+            ConnectorCollection = new();
+            ConnectorCollection.Add(new ConnectorInfo()
+            {
+                Id = Guid.NewGuid(),
+                Description = "Agilent 34401",
+                ImagePath = "Y:\\Agilent34401.jpg"
+            });
+        }
+        public ObservableCollection<ConnectorInfo> ConnectorCollection { get; set; }= new();
+    }
+
+    public class ConnectorInfo : ObservableObject
+    {
+        public Guid Id
+        {
+            get;
+            set => SetProperty(ref field, value);
+        }
+
+        public string? ImagePath
+        {
+            get;
+            set => SetProperty(ref field, value);
+        }
+
+        public string? Description
+        {
+            get;
+            set => SetProperty(ref field, value);
+        }
     }
 }
